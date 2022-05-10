9890d15eafe3efa19cfe0e06907aebeae9a5f14a
Ierotheos10 <105287381+Ierotheos10@users.noreply.github.com>
2022-05-10 18:11:58 +0300

    Ενημέρωση main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("-_-")
+})
 radio.onReceivedString(function (receivedString) {
     basic.showString("hi")
-})
-input.onButtonPressed(Button.B, function () {
-    radio.sendString("-_-")
 })
 radio.setGroup(1)
